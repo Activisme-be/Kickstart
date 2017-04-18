afc29938a47d2fc8b5f0da039277a7834a63215d
Tim Joosten <Tjoosten3@gmail.com>
2017-04-18 18:55:55 +0200

    Split out the navigation and footer

--- application/views/home.blade.php
+++ application/views/home.blade.php
@@ -10,19 +10,7 @@
     <body>
         <header>
             <nav>
-                <ul>
-                    <li><a href="{{ base_url() }}">Home</a></li>
-                    <li><a href="mailto:info@activisme.be">Contact</a></li>
-
-                    @if ($this->user)
-                        <li><a href="{{ base_url('backend') }}">Back-end</a></li>
-                        <li><a href="{{ base_url('authencation/logout') }}">Uitloggen</a></li>
-                    @else
-                        <li><a href="{{ base_url('authencation/login') }}">Log-in</a></li>
-                        <li><a href="{{ base_url('authencation/register') }}">Register</a></li>
-
-                    @endif
-                </ul>
+				@include('layouts/includes/front-end-navigation')
             </nav>
             <section>
                 <h1>ACTIVISME_BE</h1>
@@ -102,49 +90,7 @@
         </main>
 
         <footer>
-            <section>
-                <h1>Information</h1>
-                <section>
-                    <h2>Share</h2>
-                    <nav>
-                        <ul>
-                            <li><a href="http://www.facebook.com/share.php?u=http://www.activisme.be&title=activisme.be">FACEBOOK</a></li>
-                            <li><a href="http://twitter.com/intent/tweet?status=activisme.be+http://www.activisme.be">TWITTER</a></li>
-                            <li><a href="http://www.activisme.be">LINKEDIN</a></li>
-                            <li><a href="https://plus.google.com/share?url=http://www.activisme.be">GOOGLEPLUS</a></li>
-                        </ul>
-                    </nav>
-                </section>
-
-                <section>
-                    <h2>Follow</h2>
-                    <nav>
-                        <ul>
-                            <li><a href="https://www.facebook.com/ActivismeBE/" target="blank">FB ACTIVISME</a></li>
-                            <li><a href="https://twitter.com/Activisme_be" target="blank">TWITTER ACTIVISME</a></li>
-                            <li><a href="https://www.facebook.com/tom.manhaeghe.game" target="blank">FB TOM MANHAEGHE</a></li>
-                            <li><a href="https://twitter.com/manhaeghe" target="blank">TWITTER TOM MANHAEGHE</a></li>
-                            <li><a href="https://www.flickr.com/photos/activisme/" target="blank">FLICKR</a></li>
-                            <li><a href="https://www.youtube.com/channel/UCVL0CgcRZu8fgCKad5Mi9WA" target="blank">YOUTUBE</a></li>
-                        </ul>
-                    </nav>
-                </section>
-
-                <section>
-                    <h2>Links</h2>
-                    <nav>
-                        <ul>
-                            <li><a href="http://www.intal.be/" target="blank">INTAL</a></li>
-                            <li><a href="https://www.vrede.be/" target="blank">VREDE.BE</a></li>
-                            <li><a href="https://www.vredesactie.be/" target="blank">VREDESACTIE.BE</a></li>
-                            <li><a href="http://www.hartbovenhard.be/" target="blank">HART BOVEN HARD</a></li>
-                            <li><a href="http://www.solidarityforall.be" target="blank">SOLIDARITY FOR ALL</a></li>
-                            <li><a href="https://www.amnesty-international.be" target="blank">AMNESTY INTERNATIONAL</a></li>
-                        </ul>
-                    </nav>
-                </section>
-            </section>
-            <p><strong>Copyright &copy; activisme_be 2016</strong></p>
+			@include('layouts/includes/front-end-footer')
         </footer>
     </body>
 </html>
